26183997e4edc48f66e6a59ffd8eec13b05075c5
Burak <brkaycck@outlook.com>
2021-12-07 18:23:28 +0300

    RecyclerView , Data, Data Transfer.

---
+++ app/src/main/java/com/burakaycicek/landmarkbookkotlin/LandmarkAdapter.kt
@@ -0,0 +1,29 @@
+package com.burakaycicek.landmarkbookkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.burakaycicek.landmarkbookkotlin.databinding.RecyclerRowBinding
+
+class LandmarkAdapter(val landmarkList : ArrayList<Landmark>) : RecyclerView.Adapter<LandmarkAdapter.LandmarkHolder> () {
+
+    class LandmarkHolder(val binding: RecyclerRowBinding) : RecyclerView.ViewHolder (binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LandmarkHolder {
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return LandmarkHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LandmarkHolder, position: Int) {
+        holder.binding.recyclerViewTextView.text = landmarkList.get(position).name
+    }
+
+    override fun getItemCount(): Int {
+        return landmarkList.size
+    }
+
+
+}
--- app/src/main/java/com/burakaycicek/landmarkbookkotlin/MainActivity.kt
+++ app/src/main/java/com/burakaycicek/landmarkbookkotlin/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.burakaycicek.landmarkbookkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.burakaycicek.landmarkbookkotlin.databinding.ActivityDetailsBinding
+import com.burakaycicek.landmarkbookkotlin.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var  landmarkList : ArrayList<Landmark>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        landmarkList = ArrayList<Landmark>()
+        //Data
+
+        val pisa = Landmark("Pisa","Italy",R.drawable.pisa_tower)
+        val colosseum = Landmark("Colosseum","Italy",R.drawable.colosseum)
+        val pantheon = Landmark("Pantheon", "Italy",R.drawable.pantheon)
+        val florence = Landmark("Florence Cathedral","Italy",R.drawable.florence_cathedral)
+
+        landmarkList.add(pisa)
+        landmarkList.add(colosseum)
+        landmarkList.add(pantheon)
+        landmarkList.add(florence)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val landmarkAdapter = LandmarkAdapter (landmarkList)
+        binding.recyclerView.adapter = landmarkAdapter
+
+        /*
+
+        //Adapter : Layout % Data
+
+        //Mapping
+
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,landmarkList.map { landmark -> landmark.name })
+
+        binding.listView.adapter = adapter
+        binding.listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+
+            val intent = Intent(MainActivity@this,DetailsActivity::class.java)
+            intent.putExtra("landmark",landmarkList.get(position))
+            startActivity(intent)
+        }
+
+         */
+
+
     }
 }
